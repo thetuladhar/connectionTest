08e452e7c1859e8bdee452993680b89ce1a2517d
“the <prabhattuladhar@gmail.com>
2023-10-11 21:11:57 -0400

    init

---
+++ input_append_list.py
@@ -0,0 +1,20 @@
+emptyList=[]
+for i in range(5):
+    number=input("Enter a username :")
+    emptyList.append(number)
+print(emptyList)
+
+
+#Alternative Code
+'''UsernameList=[]
+number1=input("Enter a username :")
+UsernameList.append(number1)
+number2=input("Enter a username :")
+UsernameList.append(number2)
+number3=input("Enter a username :")
+UsernameList.append(number3)
+number4=input("Enter a username :")
+UsernameList.append(number4)
+number5=input("Enter a username :")
+UsernameList.append(number5)
+print(UsernameList)'''
