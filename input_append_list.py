d5081fdf2509504c42cc066d41d4029b1eb8fc8f
“the <prabhattuladhar@gmail.com>
2023-10-11 21:34:40 -0400

    Set Answer

--- input_append_list.py
+++ input_append_list.py
@@ -1,9 +1,9 @@
-emptyList=[]
+'''emptyList=[]
 for i in range(5):
     number=input("Enter a username :")
     emptyList.append(number)
 
-print(set(emptyList))
+print(set(emptyList))'''
 
 
 
@@ -20,3 +20,16 @@ UsernameList.append(number4)
 number5=input("Enter a username :")
 UsernameList.append(number5)
 print(set(UsernameList))'''
+
+#SET answer
+
+number1=input("Enter a username :")
+number2=input("Enter a username :")
+number3=input("Enter a username :")
+
+mtset=set()
+mtset.add(number1)
+mtset.add(number2)
+mtset.add(number3)
+
+print(mtset)
